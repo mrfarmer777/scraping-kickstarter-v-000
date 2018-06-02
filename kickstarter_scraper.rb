0a81cf61f1e67df1dc2ae82b35c491861c76a465
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 02:44:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,16 @@ def create_project_hash
   #projects: kickstarter.css('li.project.grid_4')
   #title: project.css('h2.bbcard_name strong a').text
   #imagelink: project.css('div.project-thumbnal a img').attribute('src').value
+  #description: project.css('p.bbcard_blurb').text
+  #location: project.css('span.location-name').text
+  #percent funded: project.css('ul.project-stats li.first.funded strong').text.gsub("%","").to_i
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project| #get the projects from parsed HTML node list, do with each...
+    projects[project]={} #initialize a project hash within the projects hash
+  end
+
+  projects
   
   
 end
