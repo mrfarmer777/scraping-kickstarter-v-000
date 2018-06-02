dde57fa5664fd843c17eac3677c40512a3508c28
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 02:48:36 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,12 +17,12 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project| #get the projects from parsed HTML node list, do with each...
     title=project.css('h2.bbcard_name strong a').text #go grab ahold of the title from the html node list
     projects[title.to_sym]={
-      :image_link => project.css('div.project-thumbnal a img').attribute('src').value, 
-      :description => project.css('p.bbcard_blurb').text, 
-      :location =>  project.css('span.location-name').text, 
+      :image_link => project.css('div.project-thumbnail a img').attribute('src').value,
+      :description => project.css('p.bbcard_blurb').text,
+      :location =>  project.css('span.location-name').text,
       :percent_funded => project.css('ul.project-stats li.first.funded strong').text.gsub("%","").to_i
     } #initialize a project hash within projects hash whose key is the project's title symbol-ized
-    
+
   end
 
   projects
